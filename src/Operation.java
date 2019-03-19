6dd3cbb80353fe9df7d2c5b6465ead43ba5663d6
Vladislav Ovchinnikov <ovchinnikovlad@gmail.com>
2019-03-19 21:22:35 +0300

    Добавил в класс Person поле с мобильным номером. Добавил конструкторы и геттеры/сеттеры для Person и Operation

--- src/Operation.java
+++ src/Operation.java
@@ -2,4 +2,35 @@ public class Operation {
     private double capital;
     private int term; // в днях
     private double interest;
+
+
+    public Operation(double capital, int term, double interest) {
+        this.capital = capital;
+        this.term = term;
+        this.interest = interest;
+    }
+
+    public double getCapital() {
+        return capital;
+    }
+
+    public void setCapital(double capital) {
+        this.capital = capital;
+    }
+
+    public int getTerm() {
+        return term;
+    }
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+    public double getInterest() {
+        return interest;
+    }
+
+    public void setInterest(double interest) {
+        this.interest = interest;
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,43 @@ abstract public class Person {
     private String name;
     private String surname;
     private int age;
+    private String mobile_number;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMobile_numver() {
+        return mobile_number;
+    }
+
+    public void setMobile_numver(String mobile_numver) {
+        this.mobile_number = mobile_numver;
+    }
 }
